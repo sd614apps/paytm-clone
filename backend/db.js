74904dba298628274a37d1dc950723696003feb5
SD614 Apps <sd614.apps@gmail.com>
2025-01-19 23:42:54 +0530

    Updated dependencies, schemas, and routes

--- backend/db.js
+++ backend/db.js
@@ -33,9 +33,25 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Create a model from the schema
+// Create a Schema for Account balance
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+
+// Create models from the above schemas
 const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
 
 module.exports = {
-	User
+	User,
+	Account,
 };
